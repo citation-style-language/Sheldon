5742172bb3d92dbf64b67e77b4f605e4b5eccb23
Sylvester Keil <sylvester@keil.or.at>
2015-07-13 16:20:52 +0200

    move github module to bot

--- lib/sheldon/bot.rb
+++ lib/sheldon/bot.rb
@@ -1,5 +1,10 @@
 require 'sinatra/base'
-require './lib/sheldon/github'
+require 'sinatra/config_file'
+require 'octokit'
+require 'logger'
+
+require './lib/sheldon/github_helper'
+require './lib/sheldon/template'
 
 module Sheldon
   class Bot < Sinatra::Base
@@ -18,8 +23,39 @@ module Sheldon
       disable :show_exceptions
     end
 
+    set :root, File.expand_path('../../..', __FILE__)
 
-    use Sheldon::Github
+    register Sinatra::ConfigFile
+    config_file 'config/github.yml'
+
+    helpers GithubHelper
+
+    set(:github, Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN']))
+    set(:hookshot) { |exp| condition { exp == hookshot? } }
+
+
+    before '/pull_request', hookshot: true do
+      logger.info "GitHub hookshot #{github_event.inspect} received"
+    end
+
+    post '/pull_request', hookshot: true do
+      return 202 if ping?
+      return 400 unless pull_request?
+
+      options = settings.pull_request[pull_request_action]
+      return 202 if options.nil?
+
+      template = Template.load options['template']
+
+      comment = github.add_comment(
+        repository['full_name'],
+        pull_request['number'],
+        template.render(pull_request))
+
+      #[201, nil, { location: comment.url }]
+      201
+    end
+
 
     get '/status' do
       'OK'
--- spec/sheldon/bot_spec.rb
+++ spec/sheldon/bot_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Sheldon
   describe Bot do
+    before do
+      stub_request :any, /api\.github\.com/
+    end
+
     it 'is ok' do
       get '/status'
       last_response.body.must_equal 'OK'
@@ -10,6 +14,31 @@ module Sheldon
     it 'is versioned' do
       get '/version'
       last_response.body.must_equal Sheldon::VERSION
+    end
+
+    it 'accepts only github hookshots' do
+      post '/pull_request'
+      last_response.status.must_equal 404
+    end
+
+    it 'accepts pull requests' do
+      hookshot '/pull_request', :pull_request
+      last_response.status.must_equal 202
+    end
+
+    it 'comments on opened pull requests' do
+      hookshot '/pull_request', :pull_request, :pull_request_opened
+      last_response.status.must_equal 201
+    end
+
+    it 'accepts pings' do
+      hookshot '/pull_request', :ping
+      last_response.status.must_equal 202
+    end
+
+    it 'does not accept other hookshots' do
+      hookshot '/pull_request'
+      last_response.status.must_equal 400
     end
   end
 end
