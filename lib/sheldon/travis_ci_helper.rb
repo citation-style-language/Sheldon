91fceb21e7851323979befd5fdb9fe3d6aa9c2ae
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-06-05 19:44:25 +0200

    all wonderfully undocumented

--- lib/sheldon/travis_ci_helper.rb
+++ lib/sheldon/travis_ci_helper.rb
@@ -53,7 +53,7 @@ module Sheldon
       if @details.nil?
         logger.info "Travis Build: get log from #{travis_payload.to_json}"
         prefix = 'sheldon:'.split('').collect{|c| "#{c}\b"}.join('')
-        url = "https://api.travis-ci.org/v3/job/#{travis_payload['id']}/log.txt"
+        url = "https://api.travis-ci.org/v3/job/#{travis_payload['matrix'][0]['id']}/log.txt"
         logger.info "Travis Build: get log from #{url}"
 
         # make sure we don't attempt again if we've not been successful before
